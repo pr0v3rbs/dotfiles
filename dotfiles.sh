526b79704a3542a36cb39e1ebc6aaf553272a43a
pr0v3rbs <rla5072@paran.com>
2020-02-19 21:53:59 +0900

    Add git-gutter

--- dotfiles.sh
+++ dotfiles.sh
@@ -14,9 +14,15 @@ git clone --depth=1 https://github.com/vim-syntastic/syntastic.git \
 git clone https://github.com/vim-airline/vim-airline \
         ~/.vim/bundle/vim-airline
 
+mkdir -p ~/.vim/pack/airblade/start
+git clone https://github.com/airblade/vim-gitgutter \
+        ~/.vim/pack/airblade/start/vim-gitgutter
+
 # setup
 cp ~/.vim/bundle/vim-nightfly/autoload/airline/themes/nightfly.vim \
         ~/.vim/bundle/vim-airline/autoload/airline/themes/
 
 sed -i "s/Search guibg=bg/Search guibg=DarkCyan/g" \
         ~/.vim/bundle/vim-nightfly/colors/nightfly.vim
+
+vim -u NONE -c "helptags vim-gitgutter/doc" -c q
