0a52a88617de70fd88fd0edb6f0ef78496505594
pr0v3rbs <pr0v3rbs@kaist.ac.kr>
2021-10-10 15:06:56 +0900

    Add update script

---
+++ update.sh
@@ -0,0 +1,13 @@
+# update
+cd ~/.fzf && git pull && ./install
+cd ~/.vim/bundle/vim-nightfly && git checkout colors/nightfly.vim && git pull
+cd ~/.vim/bundle/syntastic && git pull
+cd ~/.vim/bundle/vim-airline && git pull
+cd ~/.vim/pack/airblade/start/vim-gitgutter && git pull
+
+# setup
+cp ~/.vim/bundle/vim-nightfly/autoload/airline/themes/nightfly.vim \
+        ~/.vim/bundle/vim-airline/autoload/airline/themes/
+
+sed -i "s/Search guibg=bg/Search guibg=DarkCyan/g" \
+        ~/.vim/bundle/vim-nightfly/colors/nightfly.vim
