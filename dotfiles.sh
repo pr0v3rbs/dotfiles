3217defcd6707425579fac2ebdf0eb66e5349f5c
pr0v3rbs <pr0v3rbs@kaist.ac.kr>
2023-09-23 22:12:42 +0900

    Add tagbar

--- dotfiles.sh
+++ dotfiles.sh
@@ -4,28 +4,33 @@ sudo apt install tmux git curl vim
 
 # download
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-        curl -LSso ~/.vim/autoload/pathogen.vim https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+  curl -LSso ~/.vim/autoload/pathogen.vim \
+  https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
 git clone https://github.com/bluz71/vim-nightfly-guicolors \
-        ~/.vim/bundle/vim-nightfly
+  ~/.vim/bundle/vim-nightfly
 
 git clone --depth=1 https://github.com/vim-syntastic/syntastic.git \
-        ~/.vim/bundle/syntastic
+  ~/.vim/bundle/syntastic
 
 git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
 
 mkdir -p ~/.vim/pack/airblade/start
-git clone https://github.com/airblade/vim-gitgutter \
-        ~/.vim/pack/airblade/start/vim-gitgutter
+git clone --depth=1 https://github.com/airblade/vim-gitgutter \
+  ~/.vim/pack/airblade/start/vim-gitgutter
+
+mkdir -p ~/.vim/pack/preservim/start
+git clone --depth=1 https://github.com/preservim/tagbar \
+  ~/.vim/pack/preservim/start/tagbar
 
 # setup
 cp ~/.vim/bundle/vim-nightfly/autoload/airline/themes/nightfly.vim \
-        ~/.vim/bundle/vim-airline/autoload/airline/themes/
+  ~/.vim/bundle/vim-airline/autoload/airline/themes/
 
 sed -i "s/Search guibg=bg/Search guibg=DarkCyan/g" \
-        ~/.vim/bundle/vim-nightfly/colors/nightfly.vim
+  ~/.vim/bundle/vim-nightfly/colors/nightfly.vim
 
 vim -u NONE -c "helptags vim-gitgutter/doc" -c q
